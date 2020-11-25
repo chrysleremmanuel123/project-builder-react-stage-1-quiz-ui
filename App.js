1cb3eac26d6aefa3eb822d628872ac68f4eee007
chrysleremmanuel123 <44663517+chrysleremmanuel123@users.noreply.github.com>
2020-11-25 20:50:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import React from 'react';
+// import Home from './components/HomeComponent';
+import Quiz from './components/QuizComponent';
+import Result from './components/ResultComponent';
+import './App.css';
+import './Component.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Home /> */}
+      {/* <Quiz /> */}
+      <Result />
+    </div>
+  );
+}
+
+export default App;
